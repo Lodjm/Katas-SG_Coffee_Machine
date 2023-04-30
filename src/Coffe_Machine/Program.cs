11a3072f95239fdb08eba8da86f137fe7b1de4ef
Luc Daigremont <luc.daigremont@arolla.fr>
2023-04-30 10:57:21 +0200

    Add Main Loop to display Price

--- src/Coffe_Machine/Program.cs
+++ src/Coffe_Machine/Program.cs
@@ -1,7 +1,44 @@
-﻿internal class Program
+﻿using Coffee_Machine_MenuDisplay;
+using Coffee_Machine_MenuDisplay.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var serviceProvider = new ServiceCollection()
+            .AddScoped<IMenuDisplayService, MenuDisplayService>()
+        .BuildServiceProvider();
+
+        var menuDisplayService = serviceProvider.GetService<IMenuDisplayService>();
+        if (menuDisplayService == null)
+        {
+            Console.WriteLine("The machine can't start. Please call the HelpDesk.");
+        }
+        else
+        {
+            while(true)
+            {
+                Console.WriteLine("Choose your drink or type quit to shutdown the machine");
+
+                var readLine = Console.ReadLine();
+                if (readLine != null)
+                {
+                    if (readLine.Equals("quit", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        break;
+                    }
+                    try
+                    {
+                        var price = menuDisplayService.GetPrice(readLine);
+                        Console.WriteLine(price);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+        }
     }
 }
---
+++ src/Coffee_Machine_MenuDisplay/MenuDisplayService.cs
@@ -0,0 +1,12 @@
+﻿using Coffee_Machine_MenuDisplay.Contracts;
+
+namespace Coffee_Machine_MenuDisplay
+{
+    public class MenuDisplayService : IMenuDisplayService
+    {
+        public string GetPrice(string drinkName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
